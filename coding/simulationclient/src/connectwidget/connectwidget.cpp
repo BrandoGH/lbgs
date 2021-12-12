fc05e5d8ead3e9480b7b5730119f0964c91e34f5
BorisL <anonisBoo@gmail.com>
2021-12-13 01:36:27 +0800

    【simulationclient: 1.添加关闭连接逻辑 2.代码优化】

--- coding/simulationclient/src/connectwidget/connectwidget.cpp
+++ coding/simulationclient/src/connectwidget/connectwidget.cpp
@@ -8,6 +8,7 @@
 #include <QtWidgets/QPlainTextEdit>
 
 ConnectWidget::ConnectWidget(QWidget* parent)
+	: m_userId(0)
 {
 	initUi();
 }
@@ -20,7 +21,7 @@ const QString& ConnectWidget::getIpAddress()
 {
 	if (!m_pEditIP)
 	{
-		return QString("");
+		return QString();
 	}
 	return m_pEditIP->text();
 }
@@ -52,7 +53,9 @@ void ConnectWidget::initUi()
 			m_pEditIP = new QLineEdit(this);
 			m_pEditPort = new QLineEdit(this);
 			m_pEditConnectCount = new QLineEdit(this);
-			m_pConnectBt = new QPushButton("connect", this);
+			m_pConnectBt = new QPushButton("connect all", this);
+			m_pDisConnectBt = new QPushButton("disconnect", this);
+			m_pDisConnectBt->setEnabled(false);
 			m_pEditIP->setPlaceholderText("ip address");
 			m_pEditIP->setText("127.0.0.1");
 			m_pEditPort->setPlaceholderText("port");
@@ -64,8 +67,10 @@ void ConnectWidget::initUi()
 		m_pHLayout->addWidget(m_pEditPort);
 		m_pHLayout->addWidget(m_pEditConnectCount);
 		m_pHLayout->addWidget(m_pConnectBt);
+		m_pHLayout->addWidget(m_pDisConnectBt);
 
 		m_pMsgPlantText = new QPlainTextEdit(this);
+		m_pMsgPlantText->setPlainText("test msg");
 		m_pLogPlantText = new QPlainTextEdit(this);
 		m_pLogPlantText->setReadOnly(true);
 	}
@@ -78,17 +83,55 @@ void ConnectWidget::initUi()
 	setLayout(m_pMainVLayout);
 
 	connect(m_pConnectBt, SIGNAL(clicked(bool)), this, SLOT(onConnectBtClicked(bool)));
+	connect(m_pDisConnectBt, SIGNAL(clicked(bool)), this, SLOT(onDisConnectBtClicked(bool)));
 }
 
-void ConnectWidget::onConnectBtClicked(bool checked)
+bool ConnectWidget::connectAll()
 {
 	if (getConnectCount() <= 0)
 	{
 		m_pLogPlantText->appendPlainText("type connect cout error");
+		return false;
 	}
 
 	for (int i = 0; i < getConnectCount(); ++i)
 	{
 		User* user = new User(this);
+		user->setUserId(++m_userId);
+		m_vecUser.push_back(user);
 	}
+
+	return true;
+}
+
+void ConnectWidget::disConnectAll()
+{
+	for (int i = 0; i < m_vecUser.size(); ++i)
+	{
+		if (m_vecUser[i])
+		{
+			delete m_vecUser[i];
+			m_vecUser[i] = NULL;
+		}
+	}
+
+	m_vecUser.clear();
+}
+
+void ConnectWidget::onDisConnectBtClicked(bool checked)
+{
+	disConnectAll();
+	m_pConnectBt->setEnabled(true);
+	m_pDisConnectBt->setEnabled(false);
+}
+
+void ConnectWidget::onConnectBtClicked(bool checked)
+{
+	if (!connectAll())
+	{
+		return;
+	}
+
+	m_pConnectBt->setEnabled(false);
+	m_pDisConnectBt->setEnabled(true);
 }
--- coding/simulationclient/src/connectwidget/connectwidget.h
+++ coding/simulationclient/src/connectwidget/connectwidget.h
@@ -3,12 +3,14 @@
 
 #include "servercommon/basedef.h"
 #include <QtWidgets/QWidget>
+#include <QtCore/QVector>
 
 class QHBoxLayout;
 class QLineEdit;
 class QPushButton;
 class QVBoxLayout;
 class QPlainTextEdit;
+class User;
 
 class ConnectWidget : public QWidget
 {
@@ -26,9 +28,12 @@ public:
 
 private:
 	void initUi();
+	bool connectAll();
+	void disConnectAll();
 
 private slots:
 	void onConnectBtClicked(bool checked);
+	void onDisConnectBtClicked(bool checked);
 
 private:
 	QVBoxLayout* m_pMainVLayout;
@@ -37,8 +42,12 @@ private:
 	QLineEdit* m_pEditPort;
 	QLineEdit* m_pEditConnectCount;
 	QPushButton* m_pConnectBt;
+	QPushButton* m_pDisConnectBt;
 	QPlainTextEdit* m_pMsgPlantText;
 	QPlainTextEdit* m_pLogPlantText;
+
+	uint m_userId;
+	QVector<User*> m_vecUser;
 };
 
 #endif // !__CONNECT_WIDGET_H__
