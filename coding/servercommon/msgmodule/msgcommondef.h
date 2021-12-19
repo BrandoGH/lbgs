4dda3e634661875e61de89676e89fb4d6c025cec
BorisL <anonisBoo@gmail.com>
2021-12-19 19:22:37 +0800

    【module: servercommon】【alter: 添加报文头和报文尾结构】

---
+++ coding/servercommon/msgmodule/msgcommondef.h
@@ -0,0 +1,55 @@
+#ifndef __MSG_COMMON_DEF_H__
+#define __MSG_COMMON_DEF_H__
+#include "basedef.h"
+#include <boost/static_assert.hpp>
+
+#pragma pack(push,4)
+
+struct MsgHeader 
+{
+	MsgHeader()
+	{
+		reset();
+	}
+
+	void reset()
+	{
+		m_msgSeq = 0;
+		m_msgFlag = 0;
+		memset(m_msgLen, 0, sizeof(m_msgLen));
+		memset(m_msgType, 0, sizeof(m_msgType));
+		memset(m_msgSender, 0, sizeof(m_msgSender));
+		memset(m_msgReceive, 0, sizeof(m_msgReceive));
+		memset(m_msgForward, 0, sizeof(m_msgForward));
+		memset(m_reserve, 0, sizeof(m_reserve));
+	}
+
+	byte m_msgLen[2];
+	byte m_msgType[2];
+	byte m_msgSender[4];
+	byte m_msgReceive[4];
+	byte m_msgForward[4];
+	byte m_msgSeq;
+	byte m_msgFlag;
+	byte m_reserve[2];
+};
+BOOST_STATIC_ASSERT(sizeof(MsgHeader) == 20);
+
+struct MsgEnder
+{
+	MsgEnder()
+	{
+		reset();
+	}
+
+	void reset()
+	{
+		memset(m_md5, 0, sizeof(m_md5));
+	}
+
+	byte m_md5[16];
+};
+BOOST_STATIC_ASSERT(sizeof(MsgEnder) == 16);
+
+#pragma pack(pop)
+#endif // !__MSG_COMMON_DEF_H__
