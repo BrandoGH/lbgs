df84554ee38a6860b2e587b059218e4b78d0f41d
BorisL <anonisBoo@gmail.com>
2021-12-20 02:40:02 +0800

    【module:cmake  】【alter: 1.改为默认release模式 2.win默认开启debug选项 linux按需开启，默认最大优化】

--- coding/lbgsbuild.bat
+++ coding/lbgsbuild.bat
@@ -1,1 +1,1 @@
-cmake -S "./" -B "../build" -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Visual Studio 15 2017 Win64" %*
+cmake -S "./" -B "../build" -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 15 2017 Win64" %*
