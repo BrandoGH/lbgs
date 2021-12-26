0d3ddb02d7c1625311bd8648fffe046da39dd5f1
BorisL <anonisBoo@gmail.com>
2021-12-26 21:30:11 +0800

    【module: cmake】【alter: 指令开关添加单元测试工程】

--- coding/buildlbgslinux_rel.sh
+++ coding/buildlbgslinux_rel.sh
@@ -1,1 +1,1 @@
-/home/opt/vcpkg/downloads/tools/cmake-3.21.1-linux/cmake-3.21.1-linux-x86_64/bin/cmake -S "./" -B "../build" -DCMAKE_BUILD_TYPE=Release -DENABLE_DEBUG_LOG=OFF -DENABLE_DEBUG_MODE=OFF $*
+/home/opt/vcpkg/downloads/tools/cmake-3.21.1-linux/cmake-3.21.1-linux-x86_64/bin/cmake -S "./" -B "../build" -DCMAKE_BUILD_TYPE=Release -DENABLE_DEBUG_LOG=OFF -DENABLE_DEBUG_MODE=OFF -DENABLE_UNITEST=OFF $*
--- coding/buildlbgswin.bat
+++ coding/buildlbgswin.bat
@@ -1,1 +1,1 @@
-cmake -S "./" -B "../build" -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -DENABLE_DEBUG_LOG=ON -DOPEN_SIMULATION_CLIENT=ON %*
+cmake -S "./" -B "../build" -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -DENABLE_DEBUG_LOG=ON -DOPEN_SIMULATION_CLIENT=ON -DENABLE_UNITEST=ON %*
