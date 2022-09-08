0bb0f5a5d8c163915752aef647b690b679267421
Brando <anonisboo@gmail.com>
2022-09-09 05:29:21 +0800

    逻辑服取消设置端口的命令

--- coding/logicserver/main.cpp
+++ coding/logicserver/main.cpp
@@ -11,21 +11,6 @@
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 
-void optPort(const boost::program_options::variables_map& vm, LogicServer*& pLogicServer)
-{
-	if (vm.count("port"))
-	{
-		pLogicServer = new LogicServer(vm["port"].as<uint>());
-		printf("new LogicServer(int port)\n");
-		return;
-	}
-	pLogicServer = new LogicServer();
-	printf("new LogicServer()\n");
-}
-
-/*
-	command: logicserver --port=xxx --xxx=xxx
-*/
 int main(int argc, char* argv[])
 {
 #ifdef WIN_OS
@@ -33,41 +18,8 @@ int main(int argc, char* argv[])
 #endif
 	SystemInfoNS::g_strCurProcessName = boost::filesystem::path(argv[0]).filename().string();
 
-	LogicServer* pLogicServer = NULL;
-	boost::program_options::options_description opts("logicservser option");
-	boost::program_options::variables_map vm;
-	opts.add_options()
-		("help", "get option help")
-		("port", boost::program_options::value<uint>(), "set listen port")
-		;
-	try
-	{
-		boost::program_options::store(boost::program_options::parse_command_line(argc, argv, opts), vm);
-	} catch (...)
-	{
-		printf("WTF about your input param, please type --help\n");
-		return 0;
-	}
-
-	do
-	{
-		if (vm.empty())
-		{
-			pLogicServer = new LogicServer();
-			printf("new LogicServer()\n");
-			break;
-		}
-
-		if (vm.count("help"))
-		{
-			std::cout << opts << std::endl;
-			return 0;
-		}
-
-		optPort(vm, pLogicServer);
-	} while (0);
-
-	assert(pLogicServer);
+	LogicServer* pLogicServer = new LogicServer();
+	printf("new LogicServer()\n");
 
 	// start server
 	pLogicServer->start();
