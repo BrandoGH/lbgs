d3277278efa7b6a5c890c58fe17e7ffb6f14662e
Brando <anonisboo@gmail.com>
2022-09-15 22:23:32 +0800

    win 添加redis api

--- 3rd/x64-windows/share/hiredis/hiredis-targets-debug.cmake
+++ 3rd/x64-windows/share/hiredis/hiredis-targets-debug.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "hiredis::hiredis" for configuration "Debug"
 set_property(TARGET hiredis::hiredis APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(hiredis::hiredis PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
-  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/hiredis.lib"
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/debug/lib/hiredis.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/bin/hiredis.dll"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS hiredis::hiredis )
-list(APPEND _IMPORT_CHECK_FILES_FOR_hiredis::hiredis "${_IMPORT_PREFIX}/debug/lib/hiredis.lib" )
+list(APPEND _cmake_import_check_targets hiredis::hiredis )
+list(APPEND _cmake_import_check_files_for_hiredis::hiredis "${_IMPORT_PREFIX}/debug/lib/hiredis.lib" "${_IMPORT_PREFIX}/debug/bin/hiredis.dll" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
--- 3rd/x64-windows/share/hiredis/hiredis-targets-release.cmake
+++ 3rd/x64-windows/share/hiredis/hiredis-targets-release.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "hiredis::hiredis" for configuration "Release"
 set_property(TARGET hiredis::hiredis APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(hiredis::hiredis PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/hiredis.lib"
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/hiredis.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/hiredis.dll"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS hiredis::hiredis )
-list(APPEND _IMPORT_CHECK_FILES_FOR_hiredis::hiredis "${_IMPORT_PREFIX}/lib/hiredis.lib" )
+list(APPEND _cmake_import_check_targets hiredis::hiredis )
+list(APPEND _cmake_import_check_files_for_hiredis::hiredis "${_IMPORT_PREFIX}/lib/hiredis.lib" "${_IMPORT_PREFIX}/bin/hiredis.dll" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
