774e5b5717ac2c6583848c718570d230309d370a
Brando <anonisboo@gmail.com>
2022-09-16 02:35:15 +0800

    添加redis例子代码

--- coding/test/exampletest.cpp
+++ coding/test/exampletest.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <servercommon/boostmodule/basedef.h>
 #include <servercommon/timermodule/timer.h>
+#include <hiredis/hiredis.h>
 
 // 定时器用法
 namespace
@@ -23,9 +24,42 @@ TEST(Timer_Example, test)
 	getchar();*/
 }
 
+TEST(Redis_Example, test)
+{
+	timeval tv = { 1,500000 };
+
+	redisContext* conn = redisConnectWithTimeout("192.168.43.122", 6379, tv);
+	if (conn != NULL && conn->err)
+	{
+		printf("connection error: %s\n", conn->errstr);
+		return;
+	}
+
+	redisReply* replya = NULL;
+	replya = (redisReply*)redisCommand(conn, "AUTH default %s", "123456");
+	if (replya->type != REDIS_REPLY_ERROR)
+	{
+		printf("Redis password ok\n");
+	} else
+	{
+		printf("Redis password error\n");
+	}
+	freeReplyObject(replya);
+
+	redisReply* reply = NULL;
+	reply = (redisReply*)redisCommand(conn, "SET %s %s", "foo", "bar");
+	freeReplyObject(reply);
+
+	reply = (redisReply*)redisCommand(conn, "GET %s", "foo");
+	printf("%s\n", reply->str);
+	freeReplyObject(reply);
+
+	redisFree(conn);
+}
+
 TEST(main_test, test)
 {
-
+	
 }
 
 }
