bc005738ede3bd54d84a101d5628e8ca53a297da
Brando <anonisboo@gmail.com>
2022-09-16 20:30:41 +0800

    redis案例代码练习

--- coding/test/exampletest.cpp
+++ coding/test/exampletest.cpp
@@ -2,7 +2,12 @@
 #include <iostream>
 #include <servercommon/boostmodule/basedef.h>
 #include <servercommon/timermodule/timer.h>
+#include <boost/random.hpp>
+
+extern "C"
+{
 #include <hiredis/hiredis.h>
+}
 
 // 定时器用法
 namespace
@@ -24,9 +29,10 @@ TEST(Timer_Example, test)
 	getchar();*/
 }
 
+// link redis and set/get value
 TEST(Redis_Example, test)
 {
-	timeval tv = { 1,500000 };
+	/*timeval tv = { 1,500000 };
 
 	redisContext* conn = redisConnectWithTimeout("192.168.43.122", 6379, tv);
 	if (conn != NULL && conn->err)
@@ -54,12 +60,78 @@ TEST(Redis_Example, test)
 	printf("%s\n", reply->str);
 	freeReplyObject(reply);
 
-	redisFree(conn);
+	redisFree(conn);*/
 }
 
-TEST(main_test, test)
+// redis verification code study -- test
+TEST(Redis_Example_2, test)
 {
+	/*boost::random::mt19937_64 gen(time(NULL));
+	boost::random::uniform_int_distribution<> dist(100000,999999);
+	int r = dist(gen);
 	
+	timeval tv = { 1,500000 };
+
+	redisContext* conn = redisConnectWithTimeout("192.168.43.122", 6379, tv);
+	if (conn != NULL && conn->err)
+	{
+		printf("connection error: %s\n", conn->errstr);
+		return;
+	}
+
+	redisReply* reply = NULL;
+	reply = (redisReply*)redisCommand(conn, "AUTH default %s", "123456");
+	if (reply->type != REDIS_REPLY_ERROR)
+	{
+		printf("Redis password ok\n");
+	} else
+	{
+		printf("Redis password error\n");
+	}
+	freeReplyObject(reply);
+
+	reply = (redisReply*)redisCommand(conn, "setnx check_code %d", r);
+	freeReplyObject(reply);
+
+	reply = (redisReply*)redisCommand(conn, "setnx input_count 0");
+	freeReplyObject(reply);
+
+	reply = (redisReply*)redisCommand(conn, "expire check_code 120");
+	freeReplyObject(reply);
+
+	int intpu_count = 0;
+	reply = (redisReply*)redisCommand(conn, "get input_count");
+	intpu_count = CAST_TO(int, reply->str);
+	freeReplyObject(reply);
+	if (intpu_count == 3)
+	{
+		printf("input caount == 3\n");
+		return;
+	}
+
+	int retCode = 0;
+	reply = (redisReply*)redisCommand(conn, "get check_code");
+	retCode = CAST_TO(int, reply->str);
+	freeReplyObject(reply);
+
+	printf("please input check code: [%d]\n", retCode);
+
+	int inputCneck = 0;
+	scanf("%d",&inputCneck);
+	if (inputCneck == retCode)
+	{
+		printf("check code ok\n");
+	}
+	else
+	{
+		printf("check code error\n");
+	}
+
+	reply = (redisReply*)redisCommand(conn, "incr input_count");
+	freeReplyObject(reply);
+
+	redisFree(conn);*/
+
 }
 
 }
--- coding/test/main.cpp
+++ coding/test/main.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
 #include <servercommon/sysinfomodule/minidump/minidump.h>
 
+TEST(main_test, test)
+{
+	// example code 
+}
+
 int main(int argc, char** argv)
 {
 #ifdef WIN_OS
