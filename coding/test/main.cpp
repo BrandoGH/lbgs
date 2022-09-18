adaa34e10bb2a2bdf71dff2ad4e9e3de722b3afc
Brando <anonisboo@gmail.com>
2022-09-19 03:13:10 +0800

    unit test main_example tip

--- coding/test/main.cpp
+++ coding/test/main.cpp
@@ -1,9 +1,17 @@
 #include <gtest/gtest.h>
 #include <servercommon/sysinfomodule/minidump/minidump.h>
+#include <servercommon/basedef.h>
+
 
 TEST(main_test, test)
 {
 	// example code 
+	printf_color(PRINTF_YELLOW, "-----------------------------------------------  main_test_start ----------------------------------------------\n");
+	printf_color(PRINTF_YELLOW, "// You can write your test code here , such as \"hello world !!\" \n");
+
+
+	printf_color(PRINTF_YELLOW, "-----------------------------------------------  main_test_end   ----------------------------------------------\n");
+
 }
 
 int main(int argc, char** argv)
