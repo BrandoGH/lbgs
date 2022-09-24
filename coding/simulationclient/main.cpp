4f5f5e4d532e647ef4ab299f4723f50d9b4bc10e
Brando <anonisboo@gmail.com>
2022-09-24 12:51:07 +0800

    qt: support QUiLoader

--- coding/simulationclient/main.cpp
+++ coding/simulationclient/main.cpp
@@ -1,11 +1,13 @@
 #include "src/mainwidget.h"
-#include "servercommon/commontool/msgtool/msgtool.h"
-#include "servercommon/basedef.h"
+#include <servercommon/commontool/msgtool/msgtool.h>
+#include <servercommon/basedef.h>
 #include <servercommon/sysinfomodule/minidump/minidump.h>
 
 #include <iostream>
 #include <QtWidgets/QApplication>
 
+#include <QtUiTools/QUiLoader>
+#include <QtCore/QFile>
 
 int main(int argc, char* argv[])
 {
@@ -14,6 +16,16 @@ int main(int argc, char* argv[])
 
 	MainWidget mw;
 	mw.show();
+
+	/*QUiLoader loader;
+	QFile file("F:\\code\\lbgs\\coding\\simulationclient\\test.ui");
+	file.open(QFile::ReadOnly);
+	QWidget* widget = loader.load(&file, NULL);
+	if (widget)
+	{
+		widget->show();
+	}
+	file.close();*/
 
 	return app.exec();
 }
