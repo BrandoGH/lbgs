f0220b076cd6fc8ee5fc4c94a9637324d16662ec
Brando <anonisboo@gmail.com>
2022-09-26 15:06:09 +0800

    add test resource file

---
+++ coding/test/resource/role.sql
@@ -0,0 +1,4 @@
+INSERT INTO "role" ("name", "age") VALUES ('liubin', 12);
+INSERT INTO "role" ("name", "age") VALUES ('dengtong', 10);
+INSERT INTO "role" ("name", "age") VALUES ('jacek', 30);
+INSERT INTO "role" ("name", "age") VALUES ('steady', 700);
