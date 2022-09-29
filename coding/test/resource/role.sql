6f5ac67665678a27d51c6d045269d4acf7111012
Brando <anonisboo@gmail.com>
2022-09-29 15:26:39 +0800

    update role sql test file

--- coding/test/resource/role.sql
+++ coding/test/resource/role.sql
@@ -1,4 +1,35 @@
-INSERT INTO "role" ("name", "age") VALUES ('liubin', 12);
-INSERT INTO "role" ("name", "age") VALUES ('dengtong', 10);
-INSERT INTO "role" ("name", "age") VALUES ('jacek', 30);
-INSERT INTO "role" ("name", "age") VALUES ('steady', 700);
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local_debian_postgres
+ Source Server Type    : PostgreSQL
+ Source Server Version : 140005
+ Source Host           : 192.168.43.122:5432
+ Source Catalog        : postgres
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 140005
+ File Encoding         : 65001
+
+ Date: 29/09/2022 15:25:14
+*/
+
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."role";
+CREATE TABLE "public"."role" (
+  "name" varchar(255) COLLATE "pg_catalog"."default",
+  "age" int4
+)
+;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO "public"."role" VALUES ('liubin', 12);
+INSERT INTO "public"."role" VALUES ('dengtong', 10);
+INSERT INTO "public"."role" VALUES ('jacek', 30);
+INSERT INTO "public"."role" VALUES ('steady', 700);
