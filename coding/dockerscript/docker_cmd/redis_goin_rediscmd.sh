7a4d8d0722878b55ece758bc4080d7fbf450d9d4
brando <anonisboo@gmail.com>
2022-11-19 15:00:21 +0800

    update docker script

--- coding/dockerscript/docker_cmd/redis_goin_rediscmd.sh
+++ coding/dockerscript/docker_cmd/redis_goin_rediscmd.sh
@@ -1,4 +1,5 @@
 docker exec -it $1 redis-cli -p $2 --pass 123456 $3
 
 #$3: other option ,clulster = -c
+#$3: if need goin container $3 = /bin/bash
 #docker exec -it [name] redis-cli -p 6381 --pass 123456
